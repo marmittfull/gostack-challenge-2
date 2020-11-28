f2f313b506a6224fb3d23127ffb89bfb2035b04d
Marmittfull <tiagomarmittt@gmail.com>
2020-11-28 08:22:49 -0300

    challenge completed

---
+++ src/app.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const cors = require("cors");
+
+const { v4, validate } = require('uuid');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories)
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body
+
+  const repository = {
+    id: v4(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository)
+
+  return response.json(repository)
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if (repositoryIndex < 0)
+    return response.status(400).json({ error: "project not found" })
+
+  const likes = repositories[repositoryIndex].likes
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  }
+
+  repositories[repositoryIndex] = repository
+  return response.json(repository)
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if (repositoryIndex < 0)
+    return response.status(400).json({ error: "Project not found" })
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).send()
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  console.log(repositoryIndex);
+  if(repositoryIndex < 0)
+    return response.status(400).json({error: "Project not found"})
+
+  const repository = repositories[repositoryIndex]
+
+  repository.likes = repository.likes + 1
+
+  return response.json(repository)
+
+});
+
+module.exports = app;
